11e83fe56611150ba6fc2149db8c588af8873c55
saiteja2331 <budhisaiteja@gmail.com>
2024-12-31 15:30:47 +0530

    first commit

---
+++ SBIBank.java
@@ -0,0 +1,21 @@
+package date31;
+
+public class SBIBank {
+	public static void main(String[] args) {
+		
+		SBIBank sbi = new SBIBank();
+		
+		sbi.withDraw(10000);
+		sbi.withDraw(7000);
+		sbi.withDraw(1000);
+		
+		
+	}
+
+	private void withDraw(int i) {
+		
+		
+		
+	}
+
+}
